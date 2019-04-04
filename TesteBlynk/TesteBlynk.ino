92b76c9a1916bc25c443e7c16f1857c20e0dcaee
Joao Fracarolli <jpaulovf@gmail.com>
2019-04-04 14:32:11 -0300

    Diretórios criados com a primeira versão do código .ino e gitignore adicionado

---
+++ TesteBlynk/TesteBlynk.ino
@@ -0,0 +1,22 @@
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+char auth[] = "425cb1306b604ab49e8897b29abd0f28";
+char ssid[] = "TP-LINK_LAB-LSMO";
+char pass[] = "sensores-lsmo";
+
+BLYNK_CONNECTED(){
+  Blynk.syncVirtual(V1);
+}
+
+void setup() {
+
+  pinMode(2, OUTPUT);
+  Blynk.begin(auth, ssid, pass);
+
+}
+
+void loop() {
+  Blynk.run();
+}
