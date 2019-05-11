2cabbfa6490104c73e57b4b9fb972ff3db9a10fc
Joao Fracarolli <jpaulovf@gmail.com>
2019-05-11 14:57:34 -0300

    Migração do projeto para o PlatformIO IDE

---
+++ EnergyMeter_Pio/EnergyMeter/lib/DataStorage/DataStorage.cpp
@@ -0,0 +1,98 @@
+/*********************************************
+ * DataStorage.cpp - Rotinas para armazenamento
+ * de dados de configuração na EEPROM
+ * -------------------------------------------
+ * 
+ * autor: jpaulovf@gmail.com
+ * 
+ *********************************************/
+
+#include "DataStorage.h"
+#include <Arduino.h>
+#include <EEPROM.h>
+
+/* ------------------------------------- *
+ * Globais                               *
+ * ------------------------------------- */
+unsigned int eeprom_ready = 0; 
+
+/* ------------------------------------- *
+ * Funções                               *
+ * ------------------------------------- */
+
+/*
+ * Inicialização da EEPROM
+ */
+static void initEEPROM(){
+
+  EEPROM.begin(EEPROM_SIZE);
+  eeprom_ready = 1;
+    
+}
+
+/*
+ * Armazena dados na EEPROM
+ */
+int storeData(unsigned int addr, char *data, unsigned int size){
+
+  int i;
+
+  // Checando se a EEPROM foi inicializada
+  if (eeprom_ready == 0){
+    initEEPROM();
+  }
+
+  // Checando se o endereço é válido
+  if (addr > 512){
+    return EEPROM_BADADDR;
+  }
+
+  // Checando se o tamanho dos dados é válido
+  if (size + addr > 512){
+    return EEPROM_BADSIZE;
+  }
+
+  // Escrevendo
+  for (i=0; i<size; i++){
+    EEPROM.write(addr+i, data[i]);
+  }
+
+  // Enviando a mensagem para a EEPROM
+  EEPROM.commit();
+
+  return EEPROM_OK;
+  
+}
+
+/*
+ * Lê dados armazenados na EEPROM
+ */
+int getStoredData(unsigned int addr, char *data, unsigned int size){
+
+  int i;
+  char value;
+
+  // Checando se a EEPROM foi inicializada
+  if (eeprom_ready == 0){
+    initEEPROM();
+  }
+
+  // Checando se o endereço é válido
+  if (addr > 512){
+    return EEPROM_BADADDR;
+  }
+
+  // Checando se o tamanho dos dados é válido
+  if (size + addr > 512){
+    return EEPROM_BADSIZE;
+  }
+
+  // Lendo
+  for (i=0; i<size; i++){
+    value = (char) EEPROM.read(addr+i);
+    data[i] = value;
+  }
+
+  return EEPROM_OK;
+  
+}
---
+++ EnergyMeter_Pio/EnergyMeter/lib/UserInput/UserInput.cpp
@@ -0,0 +1,52 @@
+/*************************************
+ * UserInput.cpp -- Rotinas para entrar 
+ * com strings
+ * -----------------------------------
+ * 
+ * autor: jpaulovf@gmail.com
+ * 
+ *************************************/
+
+ #include <Arduino.h>
+ #include "UserInput.h"
+
+ void initSerial(){
+    Serial.begin(9600); 
+    Serial.setTimeout(60000);
+ }
+
+ void getUserInput(char *buf, unsigned int size){
+
+    unsigned int bufsize;
+    int i;
+    int c;
+
+    // Checando tamanho
+    if (size > BUFMAX){
+      bufsize = BUFMAX;
+    }
+    else{
+      bufsize = size;
+    }
+
+    // Limpando buffer
+    for (i=0; i<BUFMAX; i++){
+      buf[i] = 0;
+    }
+
+    // Recebendo os dados da serial e colocando no buffer
+    // Recebe até encontrar ENTER ou encher o buffer
+    Serial.readBytesUntil(0x0A, buf, bufsize);
+    
+    // Lendo o restante dos caracteres (flush)
+    while(Serial.available()){
+      Serial.read();  // Esse read nao faz nada, só tira os chars do buffer
+    }
+
+ }
+
+ 
+
+ 
+ 
+ 
